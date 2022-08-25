5e10cd207aa893c505985c1a92eca8d1b492c339
JohnZSuh <john.z.suh@gmail.com>
2022-08-24 22:45:26 -0500

    Basic groundwork for Cicada Tracker, needs scanner and iterator

---
+++ src/main/java/com/github/project0/Cicada.java
@@ -0,0 +1,81 @@
+package com.github.project0;
+import java.util.HashSet;
+import java.util.Set;
+
+class Cicada {
+    private final Set<String> listOfCicadas;
+    private final String species;
+
+    public Cicada(String species) {
+        this.species = species;
+        this.listOfCicadas = new HashSet<>();
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public Set<String> getListOfCicadas() {
+        return listOfCicadas;
+    }
+
+    public void addSpecies(String species) {
+        listOfCicadas.add(species);
+    }
+
+
+    String a1 = "Cicadettana calliope calliope";
+    String a2 = "Southern Grass Cicada";
+    String b1 = "Diceroprocta vitripennis";
+    String b2 = "Green Winged Cicada";
+    String c1 = "Magicicada cassinii";
+    String c2 = "Cassini Periodical Cicaada";
+    String c3 = "17-Year Cicada";
+    String d1 = "Magicicada neotredecim Marshall and Cooley";
+    String d2 = "13 Periodical Cicada";
+    String d3 = "13-Year Cicada";
+    String d4 = "John and David's Cicada";
+    String e1 = "Magicicada septendecim";
+    String e2 = "Decim Periodical Cicada";
+    String e3 = "Linnaeus's 17-Year Cicada";
+    String e4 = "17-Year Cicada";
+    String f1 = "Magicicada septendecula";
+    String f2 = "Decula Periodical Cicdada";
+    String f3 = "17-Year Cicada";
+    String g1 = "Magicicada tredecassini";
+    String g2 = "13-Year Cicada";
+    String g3 = "13-Year Cassini";
+    String h1 = "Magicicada tredecim";
+    String h2 = "13-Year Cicada";
+    String h3 = "13-Year Decim";
+    String i1 = "Magicicada tredecula";
+    String i2 = "13-Year Cicada";
+    String i3 = "13-Year Decula";
+    String j1 = "Megatibicen auletes";
+    String j2 = "Northern Dusk Singing Cicada";
+    String k1 = "Megatibicen dorsatus";
+    String k2 = "Bush Cicada";
+    String k3 = "Grand Western";
+    String k4 = "Giant Grassland Cicada";
+    String l1 = "Megatibicen pronotalis walkeri";
+    String l2 = "Walker's Cicada";
+    String m1 = "Neocicada hieroglyphica hieroglyphica";
+    String m2 = "Hierogylphic Cicada";
+    String n1 = "Neotibicen canicularis";
+    String n2 = "Dog-day Cicada";
+    String o1 = "Neotibicen linnei";
+    String o2 = "Linne's Cicada";
+    String p1 = "Neotibicen lyricen engelhardti";
+    String p2 = "Dark Lyric Cicada";
+    String q1 = "Neotibicen lyricen lyricen";
+    String q2 = "Lyric Cicada";
+    String r1 = "Neotibicen pruinosus pruinosus";
+    String r2 = "Scissor Grinder";
+    String s1 = "Neotibicen tibicen tibicen";
+    String s2 = "Swamp Cicada";
+    String s3 = "Morning Cicada";
+    String t1 = "Okanagana balli";
+    String u1 = "Okanagana rimosa rimoasa";
+    String u2 = "Say's Cicada";
+
+}
